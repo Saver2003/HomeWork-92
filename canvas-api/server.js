e6b2f3cd45ce6c1d8570d80edd2791723638e42d
Saver2003 <saver2003@gmail.com>
2018-06-05 17:44:26 +0600

    Initial commit

---
+++ canvas-api/server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const expressWs = require('express-ws')(app);
+
+const port = 8000;
+
+app.use(cors());
+
+const clients = {};
+let pixelDraw = [];
+
+app.ws('/canvas', (ws, req) => {
+  const id = req.get('sec-websocket-key');
+  clients[id] = ws;
+
+  console.log('some');
+  ws.send(JSON.stringify({type: 'NEW_DRAW', draw: pixelDraw}));
+
+  ws.on('message', (draw) => {
+    let decodedDraw;
+
+    try {
+      decodedDraw = JSON.parse(draw);
+    } catch (e) {
+      return ws.send(JSON.stringify({
+        type: 'ERROR'
+      }));
+    }
+
+    switch (decodedDraw.type) {
+      case 'CREATE_DRAW':
+        console.log(decodedDraw);
+        pixelDraw = pixelDraw.concat(decodedDraw.draw);
+        Object.values(clients).forEach(client => {
+          client.send(JSON.stringify({
+            type: 'NEW_DRAW',
+            draw: pixelDraw
+          }));
+        });
+        break;
+      default:
+        return ws.send(JSON.stringify({
+          type: 'ERROR'
+        }));
+    }
+
+  });
+
+  ws.on('close', () => {
+    delete clients[id];
+  })
+});
+
+
+app.listen(port, () => {
+  console.log(`Server started on ${port} port`);
+});
---
+++ canvas-front/src/App.js
@@ -0,0 +1,93 @@
+import React, {Component, Fragment} from 'react';
+import './App.css';
+
+class App extends Component {
+
+  state = {
+    mouseDown: false,
+    pixelsArray: []
+  };
+
+  componentDidMount() {
+    this.websocket = new WebSocket('ws://localhost:8000/canvas');
+
+    this.websocket.onmessage = (message) => {
+      const decodedDraw = JSON.parse(message.data);
+      console.log(decodedDraw);
+
+      this.context = this.canvas.getContext('2d');
+      this.imageData = this.context.createImageData(1, 1);
+      this.d = this.imageData.data;
+
+      this.d[0] = 200;
+      this.d[1] = 50;
+      this.d[2] = 50;
+      this.d[3] = 255;
+
+
+      decodedDraw.draw.forEach((array) => {
+        this.context.putImageData(this.imageData, array.x, array.y);
+      });
+    }
+  };
+
+
+  canvasMouseMoveHandler = event => {
+    if (this.state.mouseDown) {
+      event.persist();
+      this.setState(prevState => {
+        return {
+          pixelsArray: [...prevState.pixelsArray, {
+            x: event.clientX,
+            y: event.clientY
+          }]
+        };
+      });
+
+      this.context = this.canvas.getContext('2d');
+      this.imageData = this.context.createImageData(1, 1);
+      this.d = this.imageData.data;
+
+      this.d[0] = 200;
+      this.d[1] = 50;
+      this.d[2] = 50;
+      this.d[3] = 255;
+
+      this.context.putImageData(this.imageData, event.clientX, event.clientY);
+
+    }
+  };
+
+  mouseDownHandler = event => {
+    this.setState({mouseDown: true});
+  };
+
+  mouseUpHandler = event => {
+    this.websocket.send(JSON.stringify({
+      type: 'CREATE_DRAW',
+      draw: this.state.pixelsArray
+    }));
+    this.setState({mouseDown: false, pixelArray: []});
+  };
+
+
+  render() {
+    return (
+      <Fragment>
+        <div>
+          <canvas
+          ref={elem => this.canvas = elem}
+          style={{border: '1px solid black', background: '#eee'}}
+          width={800}
+          height={600}
+          onMouseDown={this.mouseDownHandler}
+          onMouseUp={this.mouseUpHandler}
+          onMouseMove={this.canvasMouseMoveHandler}
+          />
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
